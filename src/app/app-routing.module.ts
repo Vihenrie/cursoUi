b8de056b1731e7b2df37939d77e2e1abe02fb281
Vinicius Henrique Santos Pinto <viniciushpintolp@gmail.com>
2022-06-02 20:36:43 -0300

    programa feito em angular de curso

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+    {path: '', pathMatch: 'full', redirectTo: 'cursos'},
+    {
+      path: 'cursos',
+      loadChildren: () => import('./cursos/cursos.module').then(m => m.CursosModule)
+    }
+  ];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
